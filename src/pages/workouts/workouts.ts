94a76a1a8d20cc487f1d131c7c34e0db40d4aebf
cchardeau <corentin.chardeau@epitech.eu>
2017-11-04 23:39:04 +0100

    Add firebase

--- src/pages/workouts/workouts.ts
+++ src/pages/workouts/workouts.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController, ViewController } from 'ionic-angular';
 import { Workout } from '../../models/workout';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
     templateUrl: 'workouts-modal.html'
@@ -19,10 +20,11 @@ export class WorkoutsModal {
     templateUrl: 'workouts.html',
 })
 export class WorkoutsPage {
-
-    public workouts: [Workout];
+    //public workouts: [Workout];
+    public workouts: FirebaseListObservable<Workout>;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
+       this.workouts = this.firebaseProvider.getWorkouts();
     }
 
     ionViewDidLoad() {
@@ -33,4 +35,11 @@ export class WorkoutsPage {
         modal.present();
     }
 
+    public addWorkoutToBase(workout) {
+      this.firebaseProvider.addWorkout(this.newItem);
+    }
+
+    public removeWorkout(id) {
+      this.firebaseProvider.removeWorkout(id);
+    }
 }
